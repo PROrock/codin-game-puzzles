26bfd26376127e6ef574c8a30d637c75cc6baf81
Ondrej Jelinek <ondjelinek@csob.cz>
2020-12-11 23:37:18 +0100

    mars 2 -- better flying, but still not perfect

--- python/medium/mars-lander-2/mars2.py
+++ python/medium/mars-lander-2/mars2.py
@@ -1,20 +1,17 @@
 import math
 import sys
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
 G = 3.711  # m/s^2
 HEIGHT = 3000  # m
 WIDTH = 7000  # m
 DELTA_T = 1  # second
 P_MIN = 0
-P_MAX = 4
+P_MAX = 4  # m/s^2
 P_MAX_STEP = 1
 # A_MIN = -90
 # A_MAX = 90
-A_MIN = -25
-A_MAX = 25
+A_MIN = -45
+A_MAX = 45
 A_MAX_STEP = 15
 
 sign = lambda x: 0 if x==0 else int(math.copysign(1,x))
@@ -54,23 +51,25 @@ def findLanding(surface):
             return last, land
         last = land
 
-
 def getGoalPoint(last, land):
     return [int(math.floor((last[0]+land[0])/2)), last[1]]
 
-
 def addToPower(p, increment):
     return min(P_MAX, max(P_MIN, p+increment))
-
 
 def addToAngle(angle, increment):
     return min(A_MAX, max(A_MIN, angle+increment))
 
 
-def getAngle(lander, goalPoint):
-    # if(lander.angle != 0):
-    #     return 0;
-    diff = goalPoint[0]-lander.x
+def get_angle_increment(lander, goalPoint, hSpeed, last, land):
+    if last[0] <= lander.x <= land[0] and abs(hSpeed) <= 0:
+        diff_angle = -lander.rotate
+        return sign(diff_angle) * min(abs(diff_angle), A_MAX_STEP)
+
+    # n_turns = abs(goalPoint[0]-lander.x)//hSpeed
+
+    diff = goalPoint[0]-lander.x-15*hSpeed
+    debug(diff)
     return sign(-diff) * min(abs(diff), A_MAX_STEP)
 
 
@@ -92,12 +91,14 @@ while 1:
     X, Y, hSpeed, vSpeed, fuel, rotate, power = [int(i) for i in input().split()]
     lander = Lander(X, Y, hSpeed, vSpeed, fuel, rotate, power)
 
-    angle = addToAngle(lander.rotate, getAngle(lander, goalPoint))
+    angle_increment = get_angle_increment(lander, goalPoint, hSpeed, last, land)
+    angle = addToAngle(lander.rotate, angle_increment)
 
     if vSpeed <= (-40) or angle != 0:
         newPower = addToPower(power, +1)
     else:
         newPower = addToPower(power, -1)
 
+    debug(f"Lander rotate={lander.rotate}, angle_incr={angle_increment}")
     # rotate power. rotate is the desired rotation angle. power is the desired thrust power.
     print("{} {}".format(angle, newPower))
