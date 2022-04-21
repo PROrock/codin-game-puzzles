3c08a476abc6e136fc670563889af1872d100b43
Ondra Jelinek <ondrej.jelinek@email.cz>
2022-04-21 23:14:13 +0200

    spring 22 bot - basic structs, basic strategy - attack monster nearest base

--- python/bot/22_spring/README.md
+++ python/bot/22_spring/README.md
@@ -1,1 +1,5 @@
 # 22 spring bot contest - Notes, todos, data, ...
+
+## time spent
+21.4. 15:45 - 16:05 - prepare
+21.4. 2210 - 2310 basic structs, attack monster nearest base, enough for Wood 1 probably?
--- python/bot/22_spring/bot_22_spring.py
+++ python/bot/22_spring/bot_22_spring.py
@@ -1,12 +1,90 @@
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+import dataclasses
+import math
+import sys
+
+
+def debug(*texts):
+    print(texts, file=sys.stderr, flush=True)
+
+# todo create dataclass impl (might be faster?)
+# todo consider writing a namedtuple implementation - it enables nice tricks `v[0]` and might be faster
+class Point:
+    """Immutable 2D point"""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def l2_norm(self):
+        """
+        XXX: Consider using math.hypot(*coordinates) or math.dist(p, q), which is probably faster.
+        See https://docs.python.org/3/library/math.html#math.dist
+        For timing guide, see https://stackoverflow.com/a/24105845/2127340
+        """
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def l2_dist(self, other):
+        """
+        XXX: Consider using math.hypot(*coordinates) or math.dist(p, q), which is probably faster.
+        See https://docs.python.org/3/library/math.html#math.dist
+        For timing guide, see https://stackoverflow.com/a/24105845/2127340
+        """
+        return math.hypot(self.x-other.x, self.y-other.y)
+
+    def __repr__(self):
+        return f"P({self.x},{self.y})"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __add__(self, other_point):
+        return Point((self.x + other_point.x), (self.y+other_point.y))
+    def __sub__(self, other_point):
+        return Point((self.x - other_point.x), (self.y-other_point.y))
+    def __mul__(self, multiplier):
+        return Point(self.x * multiplier, self.y * multiplier)
+
+@dataclasses.dataclass(frozen=True)
+class Entity:
+    id: int
+    # todo enum?
+    type: int
+    """type: 0=monster, 1=your hero, 2=opponent hero"""
+    p: Point
+    shield_life: int
+    is_controlled: bool
+
+    @staticmethod
+    def create(_id, _type, x, y, shield_life, is_controlled):
+        return Entity(_id, _type, Point(x, y), shield_life, bool(is_controlled))
+
+@dataclasses.dataclass(frozen=True)
+class Monster(Entity):
+    health: int
+    vp: Point
+    near_base: bool
+    threat_for: int
+
+    @staticmethod
+    def create(_id, _type, x, y, shield_life, is_controlled, health, vx, vy, near_base, threat_for):
+        return Monster(_id, _type, Point(x, y), shield_life, bool(is_controlled), health, Point(vx, vy), bool(near_base), threat_for)
+
+
+def get_nearest_monster(p, monsters):
+    min_idx = min(range(len(monsters)), key=lambda i: monsters[i].p.l2_dist(p))
+    return monsters[min_idx]
+
 
 # base_x: The corner of the map representing your base
 base_x, base_y = [int(i) for i in input().split()]
+base_p = Point(base_x, base_y)
 heroes_per_player = int(input())  # Always 3
 
 # game loop
 while True:
+    entities = []
     for i in range(2):
         # health: Your base health
         # mana: Ignore in the first league; Spend ten mana to cast a spell
@@ -23,11 +101,23 @@ while True:
         # near_base: 0=monster with no target yet, 1=monster targeting a base
         # threat_for: Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
         _id, _type, x, y, shield_life, is_controlled, health, vx, vy, near_base, threat_for = [int(j) for j in input().split()]
-    for i in range(heroes_per_player):
+        if health < 0:
+            entity = Entity.create(_id, _type, x, y, shield_life, is_controlled)
+        else:
+            entity = Monster.create(_id, _type, x, y, shield_life, is_controlled, health, vx, vy, near_base, threat_for)
+        entities.append(entity)
 
-        # Write an action using print
-        # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    my_heroes = [e for e in entities if e.type == 1]
+    monsters = [e for e in entities if e.type == 0]
 
+    debug(my_heroes)
+    debug([m.p for m in monsters])
+    for hero in my_heroes:
+        if monsters:
+            monster = get_nearest_monster(base_p, monsters)
+            target_p = monster.p
 
-        # In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
-        print("WAIT")
+            # In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
+            print(f"MOVE {target_p.x} {target_p.y}")
+        else:
+            print("WAIT Boring!")
