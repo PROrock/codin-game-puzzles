4a24af9d9f686945c16a36e0a682b203c8007a82
Ondra Jelinek <ondrej.jelinek@email.cz>
2022-04-21 16:04:37 +0200

    spring 22 bot - starter code

---
+++ python/bot/22_spring/bot_22_spring.py
@@ -0,0 +1,33 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+# base_x: The corner of the map representing your base
+base_x, base_y = [int(i) for i in input().split()]
+heroes_per_player = int(input())  # Always 3
+
+# game loop
+while True:
+    for i in range(2):
+        # health: Your base health
+        # mana: Ignore in the first league; Spend ten mana to cast a spell
+        health, mana = [int(j) for j in input().split()]
+    entity_count = int(input())  # Amount of heros and monsters you can see
+    for i in range(entity_count):
+        # _id: Unique identifier
+        # _type: 0=monster, 1=your hero, 2=opponent hero
+        # x: Position of this entity
+        # shield_life: Ignore for this league; Count down until shield spell fades
+        # is_controlled: Ignore for this league; Equals 1 when this entity is under a control spell
+        # health: Remaining health of this monster
+        # vx: Trajectory of this monster
+        # near_base: 0=monster with no target yet, 1=monster targeting a base
+        # threat_for: Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+        _id, _type, x, y, shield_life, is_controlled, health, vx, vy, near_base, threat_for = [int(j) for j in input().split()]
+    for i in range(heroes_per_player):
+
+        # Write an action using print
+        # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+        # In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
+        print("WAIT")
