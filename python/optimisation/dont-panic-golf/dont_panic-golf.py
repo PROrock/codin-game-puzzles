c443fbe0af0bee343623ada6f083e5f4bb67d73f
Ondra Jelinek <ondrej.jelinek@email.cz>
2022-09-14 10:01:14 +0200

    don't panic golf - 276 - minified code, but I still need to shave 76 chars!

--- python/optimisation/dont-panic-golf/dont_panic-golf.py
+++ python/optimisation/dont-panic-golf/dont_panic-golf.py
@@ -1,56 +1,11 @@
-import sys
-
-
-def debug(text):
-    print(text, file=sys.stderr, flush=True)
-
-def signum(x):
-    if x > 0: return 1
-    if x < 0: return -1
-    return 0
-
-
-# nbFloors: number of floors
-# width: width of the area
-# nbRounds: maximum number of rounds
-# exitFloor: floor on which the exit is found
-# exitPos: position of the exit on its floor
-# nbTotalClones: number of generated clones
-# nbAdditionalElevators: ignore (always zero)
-# nbElevators: number of elevators
-m={}
-
-nbFloors, width, nbRounds, exitFloor, exitPos, nbTotalClones, nbAdditionalElevators, nbElevators = [int(i) for i in
-                                                                                                    input().split()]
-for i in range(nbElevators):
-    # elevatorFloor: floor on which this elevator is found
-    # elevatorPos: position of the elevator on its floor
-    elevatorFloor, elevatorPos = [int(j) for j in input().split()]
-    m[elevatorFloor]=elevatorPos
-m[exitFloor]=exitPos
-
-# game loop
+I=input
+a=lambda:[int(i)for i in I().split()]
+s=lambda x:((0,-1)[x<0],1)[x>0]
+w="WAIT"
+_,_,_,f,p,_,_,e=a()
+m=dict(a() for _ in range(e))
+m[f]=p
 while 1:
-    # cloneFloor: floor of the leading clone
-    # clonePos: position of the leading clone on its floor
-    # direction: direction of the leading clone: LEFT or RIGHT
-    cloneFloor, clonePos, direction = input().split()
-    cloneFloor = int(cloneFloor)
-    clonePos = int(clonePos)
-    dir = -1 if direction == "LEFT" else 1
-
-    debug(m)
-    debug(cloneFloor)
-    if cloneFloor == -1: # no leading clone yet -> wait
-        print("WAIT")
-        continue
-
-    targetPos = m[cloneFloor]
-    targetDir = targetPos - clonePos
-    action = "WAIT" if signum(targetDir) == signum(dir) or targetDir == 0 else "BLOCK"
-
-    debug(direction)
-    debug(dir)
-    debug(targetDir)
-
-    print(action)  # action: WAIT or BLOCK
+ z,g,d=I().split();z=int(z)
+ if z<0:print(w);continue
+ t=m[z]-int(g);print(("BLOCK",w)[s(t)==s((1,-1)[d=="LEFT"]) or t==0])
