b92b438477234e6049e714b38f83b8941a877ee6
Ondra Jelinek <ondrej.jelinek@email.cz>
2022-09-14 23:47:23 +0200

    don't panic golf - 255 - minified code, but still needs work!

--- python/optimisation/dont-panic-golf/dont_panic-golf.py
+++ python/optimisation/dont-panic-golf/dont_panic-golf.py
@@ -1,13 +1,11 @@
 I=input
-a=lambda:[int(i)for i in I().split()]
-s=lambda x:(x<0,-1)[x<0]
+a=lambda:map(int,I().split())
+s=lambda x:(x>0,-1)[x<0]
 w="WAIT"
-_,_,_,f,p,_,_,e=a()
+_,_,_,f,p,*_,e=a()
 m=dict(a() for _ in[1]*e)
 m[f]=p
 while 1:
  z,g,d=I().split();z=int(z)
  if z<0:print(w);continue
  t=m[z]-int(g);print(("BLOCK",w)[s(t)==s((1,-1)[d[0]<"R"]) or t==0])
-
-# TODO PROBABLY DIFFERENT/SHORTER ALGO is a way to go!
