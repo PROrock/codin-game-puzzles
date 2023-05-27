7209007d8628621bddf11d350485fc6fcc2616db
Ondřej Jelínek <ondrej.jelinek@email.cz>
2023-05-27 23:39:31 +0200

    2305 ants - add memory, wood2 done

--- python/bot/2305_spring-ants/README.md
+++ python/bot/2305_spring-ants/README.md
@@ -1,3 +1,4 @@
 # 2305 spring bot contest - Notes, todos, data, ...
 
 ## time spent
+27.5. 2230 - 2340 - 1h10m - git setup, wood 2 league
--- python/bot/2305_spring-ants/bot_2305_spring_ants.py
+++ python/bot/2305_spring-ants/bot_2305_spring_ants.py
@@ -32,6 +32,12 @@ class CellState:
     cell: Cell
 
 
+class Action:
+    @staticmethod
+    def line(source_id, target_id, strength):
+        return f"LINE {source_id} {target_id} {strength}"
+
+
 cells = {}
 
 number_of_cells = int(input())  # amount of hexagonal cells in this map
@@ -40,13 +46,15 @@ for i in range(number_of_cells):
     # initial_resources: the initial amount of eggs/crystals on this cell
     # neigh_0: the index of the neighbouring cell for each direction
     _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
-    cells[i] = Cell(i, Type(_type), initial_resources, tuple())
+    cells[i] = Cell(i, Type(_type), initial_resources, None)
 number_of_bases = int(input())
 my_bases = [int(i) for i in input().split()]
 opp_bases = [int(i) for i in input().split()]
 # debug(f"{my_bases=}")
 # debug(f"{opp_bases=}")
 
+my_base = my_bases[0]
+target_crystal = None
 # game loop
 while True:
     cell_states = {}
@@ -57,10 +65,9 @@ while True:
         resources, my_ants, opp_ants = [int(j) for j in input().split()]
         cell_states[i] = CellState(i, resources, my_ants, opp_ants, cells[i])
 
-    my_base = my_bases[0]
-    crystals = [cell_state for cell_state in cell_states.values() if cell_state.resources and cell_state.cell.type == Type.CRYSTAL]
-    crystal_state = max(crystals, key=lambda state: state.resources)
-    print(f"LINE {my_base} {crystal_state.id} {1}")
+    if target_crystal is None or not cell_states[target_crystal.id].resources:
+        crystals = [cell_state for cell_state in cell_states.values() if cell_state.resources and cell_state.cell.type == Type.CRYSTAL]
+        target_crystal = max(crystals, key=lambda state: state.resources)
 
+    print(Action.line(my_base, target_crystal.id, 1))
     # WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
-    # print("WAIT")
