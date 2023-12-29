e7f378fd7fd8d067532bf4bcb2d542340fbd056b
Ondrej Jelinek <ondrej.jelinek@email.cz>
2023-12-29 01:07:00 +0100

    23 fish - starter code

---
+++ python/bot/2312_fish_scan/2312_fish_scan.py
@@ -0,0 +1,43 @@
+import sys
+import math
+# Score points by scanning valuable fish faster than your opponent.
+
+creature_count = int(input())
+for i in range(creature_count):
+    creature_id, color, _type = [int(j) for j in input().split()]
+
+# game loop
+while True:
+    my_score = int(input())
+    foe_score = int(input())
+    my_scan_count = int(input())
+    for i in range(my_scan_count):
+        creature_id = int(input())
+    foe_scan_count = int(input())
+    for i in range(foe_scan_count):
+        creature_id = int(input())
+    my_drone_count = int(input())
+    for i in range(my_drone_count):
+        drone_id, drone_x, drone_y, emergency, battery = [int(j) for j in input().split()]
+    foe_drone_count = int(input())
+    for i in range(foe_drone_count):
+        drone_id, drone_x, drone_y, emergency, battery = [int(j) for j in input().split()]
+    drone_scan_count = int(input())
+    for i in range(drone_scan_count):
+        drone_id, creature_id = [int(j) for j in input().split()]
+    visible_creature_count = int(input())
+    for i in range(visible_creature_count):
+        creature_id, creature_x, creature_y, creature_vx, creature_vy = [int(j) for j in input().split()]
+    radar_blip_count = int(input())
+    for i in range(radar_blip_count):
+        inputs = input().split()
+        drone_id = int(inputs[0])
+        creature_id = int(inputs[1])
+        radar = inputs[2]
+    for i in range(my_drone_count):
+
+        # Write an action using print
+        # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+        # MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
+        print("WAIT 1")
