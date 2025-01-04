ab1380e48dfcfc9e9dd301a35e24ed675854d21a
Ondrej Jelinek <ondrej.jelinek@email.cz>
2025-01-04 22:44:42 +0100

    2412 celluarena - wood 4 done

--- python/bot/2412_cellularena/2412_cellularena.py
+++ python/bot/2412_cellularena/2412_cellularena.py
@@ -1,14 +1,57 @@
 import sys
 import math
+from dataclasses import dataclass
+from typing import NamedTuple
 
-# Grow and multiply your organisms to end up larger than your opponent.
 
-# width: columns in the game grid
-# height: rows in the game grid
+def debug(*s):
+    print(*s, file=sys.stderr, flush=True)
+
+class Vect(NamedTuple):
+    x: int
+    y: int
+
+    def invert(self):
+        return Vect(-self.x, -self.y)
+
+    def __add__(self, other):
+        return Vect(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vect(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, multiplier):
+        return Vect(self.x * multiplier, self.y * multiplier)
+
+    def l1_dist(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+    def l2_dist(self, other):
+        result_vect = other - self
+        return math.hypot(result_vect)
+
+@dataclass
+class Entity:
+    pos: Vect
+    type_: str
+    owner: int
+    organ_id: int
+    organ_dir: str
+    organ_parent_id: int
+    organ_root_id: int
+
+def a_grow(id_, pos, type_):
+    return f"GROW {id_} {pos.x} {pos.y} {type_}"
+
+def argmin(iterable):
+    return min(enumerate(iterable), key=lambda t:t[1])[0]
+
+
 width, height = [int(i) for i in input().split()]
 
 # game loop
 while True:
+    entities = []
     entity_count = int(input())
     for i in range(entity_count):
         inputs = input().split()
@@ -20,14 +63,16 @@ while True:
         organ_dir = inputs[5]  # N,E,S,W or X if not an organ
         organ_parent_id = int(inputs[6])
         organ_root_id = int(inputs[7])
+        entities.append(Entity(Vect(x,y), _type, owner, organ_id, organ_dir, organ_parent_id, organ_root_id))
+
     # my_d: your protein stock
     my_a, my_b, my_c, my_d = [int(i) for i in input().split()]
     # opp_d: opponent's protein stock
     opp_a, opp_b, opp_c, opp_d = [int(i) for i in input().split()]
     required_actions_count = int(input())  # your number of organisms, output an action for each one in any order
     for i in range(required_actions_count):
+        my_org = [e for e in entities if e.owner == 1][0]
+        first_p = [e for e in entities if e.type_ == "A"][0]
 
-        # Write an action using print
-        # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-        print("WAIT")
+        print(a_grow(my_org.organ_id, first_p.pos, "BASIC"))
+        # print("WAIT")
