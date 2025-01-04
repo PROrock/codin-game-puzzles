5c2eb3dc1182e8f12661ca421a1a8c629169827a
Ondrej Jelinek <ondrej.jelinek@email.cz>
2025-01-04 23:27:52 +0100

    2412 celluarena - wood 3 done (HARVESTER)

--- python/bot/2412_cellularena/2412_cellularena.py
+++ python/bot/2412_cellularena/2412_cellularena.py
@@ -7,6 +7,13 @@ from typing import NamedTuple
 def debug(*s):
     print(*s, file=sys.stderr, flush=True)
 
+def signum(x):
+    if x > 0:
+        return 1
+    if x < 0:
+        return -1
+    return 0
+
 class Vect(NamedTuple):
     x: int
     y: int
@@ -30,6 +37,27 @@ class Vect(NamedTuple):
         result_vect = other - self
         return math.hypot(result_vect)
 
+    def to_unit(self):
+        return Vect(signum(self.x), signum(self.y))
+
+DIR_TO_VECT = {
+    "N": Vect(0, -1),
+    "E": Vect(1, 0),
+    "S": Vect(0, 1),
+    "W": Vect(-1, 0),
+}
+VECT_TO_DIR = {v:k for k,v in DIR_TO_VECT.items()}
+DIRS_CLOCKWISE = list(DIR_TO_VECT.keys())
+VECTS_CLOCKWISE = list(DIR_TO_VECT.values())
+
+def expand(pos):
+    children = []
+    for dir_ in VECTS_CLOCKWISE:
+        new_pos = pos + dir_
+        if new_pos not in entity_poss:
+            children.append(new_pos)
+    return children
+
 @dataclass
 class Entity:
     pos: Vect
@@ -40,8 +68,9 @@ class Entity:
     organ_parent_id: int
     organ_root_id: int
 
-def a_grow(id_, pos, type_):
-    return f"GROW {id_} {pos.x} {pos.y} {type_}"
+def a_grow(id_, pos, type_, dir_=None):
+    d = dir_ if dir_ else ""
+    return f"GROW {id_} {pos.x} {pos.y} {type_} {d}"
 
 def argmin(iterable):
     return min(enumerate(iterable), key=lambda t:t[1])[0]
@@ -50,6 +79,21 @@ def argmin(iterable):
 width, height = [int(i) for i in input().split()]
 
 # game loop
+def get_nearest(entities, pos):
+    amin = argmin([pos.l1_dist(e.pos) for e in entities])
+    return entities[amin]
+
+
+def find_org_and_free_pos(my_orgs, entity_poss):
+    for o in my_orgs:
+        children = expand(o.pos)
+        for c in children:
+            if c not in entity_poss:
+                return o, c
+    return None, None
+
+
+
 while True:
     entities = []
     entity_count = int(input())
@@ -65,14 +109,38 @@ while True:
         organ_root_id = int(inputs[7])
         entities.append(Entity(Vect(x,y), _type, owner, organ_id, organ_dir, organ_parent_id, organ_root_id))
 
+    wall_list = [e for e in entities if e.type_ == "WALL"]
+    wall_poss = {w.pos for w in wall_list}
+    entity_poss = {e.pos for e in entities}
+
     # my_d: your protein stock
     my_a, my_b, my_c, my_d = [int(i) for i in input().split()]
     # opp_d: opponent's protein stock
     opp_a, opp_b, opp_c, opp_d = [int(i) for i in input().split()]
     required_actions_count = int(input())  # your number of organisms, output an action for each one in any order
     for i in range(required_actions_count):
-        my_org = [e for e in entities if e.owner == 1][0]
-        first_p = [e for e in entities if e.type_ == "A"][0]
+        a_proteins = [e for e in entities if e.type_ == "A"]
+        # first_p = a_proteins[0] if len(a_proteins) else opp_es[0]
+        first_p = a_proteins[0] if len(a_proteins) else None
 
-        print(a_grow(my_org.organ_id, first_p.pos, "BASIC"))
+        my_orgs = [e for e in entities if e.owner == 1]
+        my_harvesters = [e for e in my_orgs if e.type_ == "HARVESTER"]
+        opp_orgs = [e for e in entities if e.owner == 0]
+
+        my_org = [e for e in entities if e.owner == 1][0]
+        nearest_my_org = get_nearest(my_orgs, first_p.pos)
+        if nearest_my_org.pos.l1_dist(first_p.pos) == 2:
+            dir_pos = (first_p.pos - nearest_my_org.pos).to_unit()
+            if dir_pos.x != 0:
+                dir_pos = Vect(dir_pos.x, 0)
+
+            next_pos = (nearest_my_org.pos + dir_pos)
+            d = VECT_TO_DIR[(first_p.pos-next_pos)]
+            print(a_grow(nearest_my_org.organ_id, next_pos, "HARVESTER", d))
+        elif len(my_harvesters):
+            growing_organ, free_pos = find_org_and_free_pos(my_orgs, entity_poss)
+            print(a_grow(growing_organ.organ_id, free_pos, "BASIC", ))
+        else:
+            target = first_p if first_p is not None else opp_orgs[0]
+            print(a_grow(nearest_my_org.organ_id, target.pos, "BASIC", ))
         # print("WAIT")
---
+++ python/bot/2412_cellularena/README.md
@@ -0,0 +1,6 @@
+# Cellularena
+## Time log
+4.1.2025
+wood 4: 25 min
+wood 3: 40 min
+
